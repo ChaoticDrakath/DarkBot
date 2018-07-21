361aaa92379736b03050ae75a207692f603a7a84
BestCoderz <utk.bkkg@gmail.com>
2018-07-21 11:18:18 +0530

    Update boy.py

--- boy.py
+++ boy.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import random
 
 description = '''This bot is cool and made by DarkLegened'''
-bot = commands.Bot(command_prefix='d!', description=description)
+bot = commands.Bot(command_prefix='d!', description='Thanks for using DarkBot')
 
 @bot.event
 async def on_ready():
