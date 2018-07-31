f2c2a2106ea0df64001632d6fb7edec8fc642d5b
BestCoderz <utk.bkkg@gmail.com>
2018-07-31 19:57:51 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -83,7 +83,7 @@ async def poll(ctx, question, *options: str):
             return
 
         if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
-            reactions = ['?', '?']
+            reactions = ['👍', '👎']
         else:
             reactions = ['1\u20e3', '2\u20e3', '3\u20e3', '4\u20e3', '5\u20e3', '6\u20e3', '7\u20e3', '8\u20e3', '9\u20e3', '\U0001f51f']
 
