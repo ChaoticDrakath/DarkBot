87f672d2dfd69cc7763e26c72fce12a5ded4bde6
BestCoderz <utk.bkkg@gmail.com>
2018-08-03 18:08:35 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -52,6 +52,17 @@ async def on_member_join(member):
      nickname = '[GOG]' + member.name
      await client.change_nickname(member, nickname)
     
+@client.event
+async def on_member_join(member):
+    print("In our server" + member.name + " just joined")
+    r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
+    embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
+    embed.set_author(name='Welcome message')
+    embed.add_field(name = '__Welcome to Our Server__',value ='**Hope you will be active here. Check Our server rules and never try to break any rules.**',inline = False)
+    embed.set_image(url = 'https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif')
+    await client.send_message(member,embed=embed)
+    print("Sent message to " + member.name)
+    
 @client.command(pass_context = True)
 @commands.has_permissions(kick_members=True)     
 async def userinfo(ctx, user: discord.Member):
