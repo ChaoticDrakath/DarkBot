d26f155ae60c3b115108286f4e3915332de61975
BestCoderz <utk.bkkg@gmail.com>
2018-08-05 17:07:16 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -27,29 +27,7 @@ def is_owner(ctx):
 @commands.check(is_owner)
 async def shutdown():
     await client.logout()
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('d!guess'):
-        await client.send_message(message.channel, 'Guess a number between 1 to 10')
-
-        def guess_check(m):
-            return m.content.isdigit()
-
-        guess = await client.wait_for_message(timeout=5.0, author=message.author, check=guess_check)
-        answer = random.randint(1, 10)
-        if guess is None:
-            fmt = 'Sorry, you took too long. It was {}.'
-            await client.send_message(message.channel, fmt.format(answer))
-            return
-        if int(guess.content) == answer:
-            await client.send_message(message.channel, 'You are right!')
-        else:
-            await client.send_message(message.channel, 'Sorry. It is actually {}.'.format(answer))
-    
+ 
 
 @client.event
 async def on_member_join(member):
