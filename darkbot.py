413a1e06f4236c1bf30b2b28db5fecb9c13eaa9a
BestCoderz <utk.bkkg@gmail.com>
2018-08-11 19:59:23 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -14,7 +14,7 @@ async def status_task():
     while True:
         await client.change_presence(game=discord.Game(name='for d!help'))
         await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(name='with 'str(len(set(client.get_all_members())))' users'))
+        await client.change_presence(game=discord.Game(name='with '+str(len(set(client.get_all_members())))+' users'))
         await asyncio.sleep(5)
         await client.change_presence(game=discord.Game(name='in '+str(len(client.servers))+' servers'))
         await asyncio.sleep(5)
