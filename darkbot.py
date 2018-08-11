e49111d7f6a6c596ceee685622e46fb22453c027
BestCoderz <utk.bkkg@gmail.com>
2018-08-11 09:39:23 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -170,7 +170,7 @@ async def help(ctx):
     embed.set_image(url = 'https://image.ibb.co/caM2BK/help.gif')
     embed.add_field(name = '``Our Help Server Link`` ',value ='https://discord.gg/vMvv5rr',inline = False)
     embed.add_field(name = 'd!help ',value ='Explaines all the commands',inline = False)
-    embed.add_field(name = 'd!guess ',value ='To play guess game',inline = False)
+    embed.add_field(name = 'd!guess ',value ='To play guess game use ``d!guess <number>``',inline = False)
     embed.add_field(name = 'd!donate ',value ='Use it to donate us and get a special post on Official DarkBot server.',inline = False)
     embed.add_field(name = 'd!ownerinfo ',value ='To get basic information about owner.',inline = False)
     embed.add_field(name = 'd!sourcecode ',value ='Use it to see darkbot sourcecode.',inline = False)
@@ -469,6 +469,6 @@ async def guess(ctx, number):
     except ValueError:
         await client.say("Invalid number")
     else:
-        await client.say(str(arg))
+        await client.say('The correct answer is' + str(arg))
         
 client.run(os.getenv('Token'))
