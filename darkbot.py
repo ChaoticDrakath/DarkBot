2c5f43e3acf8c29456f0902df4d559d9ffd57cca
BestCoderz <utk.bkkg@gmail.com>
2018-08-16 18:30:45 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -1,7 +1,4 @@
 import discord
-from profanity import profanity
-from tinydb import TinyDB, Query
-from tinydb.operations import delete,increment
 from discord.ext.commands import Bot
 from discord.ext import commands
 import platform
