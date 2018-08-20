f6b262cb3b970e54f0e7507d8b3b8d20b65699b9
BestCoderz <utk.bkkg@gmail.com>
2018-08-20 19:09:12 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -402,15 +402,16 @@ async def ban(ctx,user:discord.Member):
 
 @client.command(pass_context = True)
 async def mute(ctx, member: discord.Member):
-     if user.server_permissions.kick_members:
+
+    if user.server_permissions.kick_members:
         await client.say('**He is mod/admin and i am unable to mute him/her**')
         return
-     try:
-	role = discord.utils.get(member.server.roles, name='Muted')
+    try:
+        role = discord.utils.get(member.server.roles, name='Muted')
         await client.add_roles(member, role)
         embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
         await client.say(embed=embed)
-     else:
+    else:
         embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
         await client.say(embed=embed)
         
