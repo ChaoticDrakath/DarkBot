7ffabc7594a80ceb7dc3ea266b97b76c9fe80bfd
BestCoderz <utk.bkkg@gmail.com>
2018-08-22 11:53:08 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -44,7 +44,13 @@ def is_dark(ctx):
 @commands.check(is_owner)
 async def restart():
     await client.logout()
- 	
+
+@client.event
+async def on_message(message):
+	await client.process_commands(message)
+
+
+
 @client.event
 async def on_member_join(member):
     print("In our server" + member.name + " just joined")
@@ -63,6 +69,37 @@ async def on_member_join(member):
     embed.set_image(url = 'https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif')
     embed.set_thumbnail(url=member.avatar_url)
     await client.send_message(channel, embed=embed)
+
+@client.command(pass_context = True)
+async def play(ctx, *, url):
+    author = ctx.message.author
+    voice_channel = author.voice_channel
+    try:
+        vc = await client.join_voice_channel(voice_channel)
+        msg = await client.say("Loading...")
+        player = await vc.create_ytdl_player("ytsearch:" + url)
+        player.start()
+        await client.say("Succesfully Loaded ur song!")
+        await client.delete_message(msg)
+    except Exception as e:
+        print(e)
+        await client.say("Reconnecting")
+        for x in client.voice_clients:
+            if(x.server == ctx.message.server):
+                await x.disconnect()
+                nvc = await client.join_voice_channel(voice_channel)
+                msg = await client.say("Loading...")
+                player2 = await nvc.create_ytdl_player("ytsearch:" + url)
+                player2.start()
+
+
+@client.command(pass_context = True)
+async def stop(ctx):
+    for x in client.voice_clients:
+        if(x.server == ctx.message.server):
+            return await x.disconnect()
+
+    return await client.say("I am not playing anyting???!")
 
 @client.command(pass_context=True)
 @commands.has_permissions(kick_members=True)
