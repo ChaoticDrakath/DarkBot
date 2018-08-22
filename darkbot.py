c92d406f8e95a4c174b9813bba9b0e413f428818
BestCoderz <utk.bkkg@gmail.com>
2018-08-22 12:05:25 +0530

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -9,7 +9,6 @@ import os
 import time
 from discord.voice_client import VoiceClient
 from discord import Game, Embed, Color, Status, ChannelType
-import PyNaCl
 
 
 Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
